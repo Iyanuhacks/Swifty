1c4edcb877e0534b5e47547de7a0c4a09086d813
Iyanuhacks <iyanuhacks@gmail.com>
2023-05-15 19:26:22 +0100

    fixed

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
