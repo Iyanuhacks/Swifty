f452c5dbd0fc009c9bf7b68a8d880c21150e0d26
Iyanuhacks <iyanuhacks@gmail.com>
2023-05-28 01:51:47 +0100

    First commit

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
-python3 manage.py runserver
+#python3 manage.py runserver
 python3 requester.py
